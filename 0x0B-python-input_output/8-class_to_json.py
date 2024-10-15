3264e9da431b6197546febb7b34819d660de9223
Rediet Geremew <redietgeremew18@gmail.com>
2024-10-15 00:12:29 -0700

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def class_to_json(obj):
+    """
+    Returns the dictionary description with simple data structure for JSON serialization
+    of an object.
+    
+    Args:
+        obj: An instance of a class.
+    
+    Returns:
+        A dictionary representation of the object.
+    """
+    return obj.__dict__.copy()
