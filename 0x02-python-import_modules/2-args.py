49a59a06d3cc25daef4a703da003340b8383be56
Rediet Geremew <redietgeremew18@gmail.com>
2024-10-21 02:53:15 -0700

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    arg_count = len(argv)
+
+    if arg_count == 0:
+        print(f"{arg_count} arguments.")
+    elif arg_count == 1:
+        print(f"{arg_count} argument:")
+    else:
+        print(f"{arg_count} arguments:")
+
+    for i, arg in enumerate(argv, 1):
+        print(f"{i}: {arg}")
